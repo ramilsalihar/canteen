d032e83ab73ef5897241318d560b884b768705ed
Rambo <ramilsalihar@gmail.com>
2024-01-26 19:44:18 +0600

    build architecture

---
+++ lib/presentation/application/application.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CanteenApp extends StatelessWidget {
+  const CanteenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Canteen App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
